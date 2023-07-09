732790b6a0cb1de5417baba84ce7be927ce187e2
S-Patriarch <apsk0529-2@mail.ru>
2023-07-09 18:51:28 +0300

    изменено 09.07.2023

--- pexcept.h
+++ pexcept.h
@@ -1,14 +1,9 @@
 // -*- C++ -*-
-
-/*
- * Copyright (c) S-Patriarch, 2023
- *
- * Описание библиотеки для работы с исключениями - exception.
- */
-
-/**
- *  (PTL) Patriarch library : pexcept.h
- */
+//
+// Copyright (c) S-Patriarch, 2023
+// Работа с исключениями - exception.
+//
+// (PTL) Patriarch library : pexcept.h
 
 #pragma once
 #if !defined( __PTL_PEXCEPT_H__ )
@@ -16,27 +11,26 @@
 
 #include <exception>
 
-/* 
- * Пример использования данного класса исключений.
- * В коде программы бросается исключение следующим образом.
- * @code
- *   throw ptl::pexception( "E: Message to exception." );
- * @endcode
- * 
- * Ловится данное исключение следующим образом.
- * @code
- *   try
- *     {
- *     throw ...
- *     }
- *   catch ( std::exception& ex )
- *     {
- *     std::cout << "Поймано исключение: " 
- *               << ex.what() 
- *               << std::endl;
- *     }
- * @endcode
- */
+// Пример использования данного класса исключений.
+// В коде программы бросается исключение следующим образом.
+// @code
+//   throw ptl::pexception( "E: Message to exception." );
+// @endcode
+// 
+// Ловится данное исключение следующим образом.
+// @code
+//   try
+//     {
+//     throw ...
+//     }
+//   catch( std::exception& ex )
+//     {
+//     std::cout 
+//       << "Поймано исключение: " 
+//       << ex.what() 
+//       << std::endl;
+//     }
+// @endcode
 
 namespace ptl
   {
@@ -44,24 +38,23 @@ namespace ptl
   class pexception final 
     : public std::exception
     {
-    private:
-      const char*  _M_message{ }; // Сообщение для исключения
-
     public:
       pexception( const char* __message )
         : std::exception(), 
-        _M_message( __message ) 
+          _M_message( __message ) 
         { }
 
-      ~pexception() 
+      ~pexception() noexcept
         { }
 //--------------------------------------------------------------------
-      /* 
-       * Перегрузка метода what() стандартного класса exception.
-       */
+// Перегрузка метода what() стандартного класса exception.
+//
       virtual const char*
       what() const noexcept override
         { return _M_message; }
+
+    private:
+      const char*  _M_message{ }; // Сообщение для исключения
     };
 
   } // namespace ptl
